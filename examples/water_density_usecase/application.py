9c76d784bab007bd7dfb148e379bcc33baa6d3fd
xAlessandroC <alexcalvio@hotmail.it>
2023-08-14 10:49:13 +0200

    example - add ontologies

--- examples/water_density_usecase/application.py
+++ examples/water_density_usecase/application.py
@@ -1,12 +1,10 @@
 import os
-
 from tripper import Triplestore
-from ontoflow.engine import OntoFlowDMEngine, OntoFlowEngineMappingStep, OntoFlowEngineValue
+from ontoflow.engine import OntoFlowDMEngine
 from ontoflow.mco_strategies.minimumCostStrategy import MinimumCostStrategy
-from tripper.mappings import mapping_routes as tripper_mapping_routes
 
 
-ONTOLOGY_FOLDER = r"C:\Users\alexc\Desktop\Universita\Ricerca\OpenModel\ExecFlowDemo\OntoKB"
+ONTOLOGY_FOLDER = __file__
 ONTOLOGY_FILENAME = "openmodel-inferred.ttl"
 INDIVIDUALS_FILENAME = "individuals2.ttl"
 
@@ -21,8 +19,8 @@ if __name__ == "__main__":
     ts = Triplestore(backend="stardog", triplestore_url="http://localhost:5820", database="d55_usecase")
 
     # Add ontology and individuals
-    ts.parse(os.path.join(ONTOLOGY_FOLDER, ONTOLOGY_FILENAME), format="turtle")
-    ts.parse(os.path.join(ONTOLOGY_FOLDER, INDIVIDUALS_FILENAME), format="turtle")
+    ts.parse(os.path.join(ONTOLOGY_FOLDER, "..", ONTOLOGY_FILENAME), format="turtle")
+    ts.parse(os.path.join(ONTOLOGY_FOLDER, "..", INDIVIDUALS_FILENAME), format="turtle")
 
 
     engine = OntoFlowDMEngine(triplestore = ts, cost_file = r"C:\Users\alexc\Desktop\Universita\Ricerca\OpenModel\OntoFlow\examples\datamodel_generation\cost_definitions.yaml", mco_interface = mco_strategy)
@@ -32,10 +30,3 @@ if __name__ == "__main__":
     route = engine.getmappingroute(target_IRI, sources_IRIs)
 
     print(route[target_IRI].show())
-
-
-
-    # route = tripper_mapping_routes(target_IRI, sources_IRIs, triplestore=ts, mappingstep_class=OntoFlowEngineMappingStep, value_class=OntoFlowEngineValue)
-
-
-    # print(route.show())
