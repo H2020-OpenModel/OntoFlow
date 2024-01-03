87e9f60764f140c12e1fd9ada36bfeece8a6dca9
Michele Brigadoi <michele.brigadoi@unibo.it>
2024-01-03 16:16:19 +0100

    Test commit and comments.

--- ontoflow/engine.py
+++ ontoflow/engine.py
@@ -1,3 +1,11 @@
 # TODO test ricerca 
 # 1. Creare ontologia come foto
 # 2. Creare query per ottenere individuals da Density
+
+# Definizione interfaccia per chiamare metodo
+# Input: ex. Density (root, albero parte, si lavora "al contrario", si parte da output e si ottengono input)
+# Output: tutto il precorso trovato (struttura ad albero con input, nodi intermedi, e valore finale)
+# Risultato: v. Public/Deliverable5.5/ontoKB/ontoflow_output.txt formato yaml
+# Vedi lookup table in tripper/mappings
+# Per interfacciamento a Triplestore usare Tripper
+# Testare in cartella di examples
