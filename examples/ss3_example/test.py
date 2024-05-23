1f3a3300391c47d5a3d6a3e47ad1b39b2cb8c958
mbrigadoi <michele.brigadoi@unibo.it>
2024-05-23 17:56:00 +0200

    Fix pybacktrip and mods repo, update converters to float, update ss3_example ontology.

--- examples/ss3_example/test.py
+++ examples/ss3_example/test.py
@@ -8,8 +8,7 @@ from tripper import Triplestore
 # podman run -i --rm -p 3030:3030 -v databases:/fuseki/databases -t fuseki --update --loc databases/openmodel /openmodel
 
 # Initialize the knowledge base
-ONTOLOGY_PATH = os.path.join(Path(os.path.abspath(__file__)).parent, "ss3_v2.ttl")
-
+ONTOLOGIES = ["ss3.ttl"]
 ROOT = "http://open-model.eu/ontologies/ss3#FenicsOutput"
 MCO = "mods"
 
@@ -22,17 +21,19 @@ ts.remove_database(
     backend="fuseki", triplestore_url=__triplestore_url, database="openmodel"
 )
 
-ts.parse(ONTOLOGY_PATH, "turtle")
+for ontology in ONTOLOGIES:
+    ts.parse(os.path.join(Path(os.path.abspath(__file__)).parent, ontology), "turtle")
 
-ts.bind("emmo", "http://emmo.info/emmo#")
+ts.bind("emmo", "https://w3id.org/emmo#")
 ts.bind("ss3", "http://open-model.eu/ontologies/ss3#")
 
 # Initialize the engine
 engine = OntoFlowEngine(triplestore=ts)
 
 kpis = [
-    {"name": "Cost1", "weight": 1, "maximise": True},
-    {"name": "Cost2", "weight": 3, "maximise": False},
+    {"name": "Accuracy", "weight": 1, "maximise": True},
+    {"name": "SimulationTime", "weight": 1, "maximise": False},
+    {"name": "OpenSource", "weight": 1, "maximise": True},
 ]
 
 # Get the best route
