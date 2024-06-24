ca973d59fc29d54197cb9e2ce3ae3da645523e1d
mbrigadoi <michele.brigadoi@unibo.it>
2024-06-24 15:43:20 +0200

    Small refactoring.

--- ontoflow/log/logger.py
+++ ontoflow/log/logger.py
@@ -3,13 +3,15 @@ import logging
 
 logging.basicConfig(
     level=os.getenv("LOGGING_LEVEL", "INFO"),
-    format='%(asctime)s - [%(levelname)s]: %(message)s'
+    format="%(asctime)s - [%(levelname)s]: %(message)s",
 )
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.getLevelName(os.getenv("LOGGING_LEVEL", "INFO")))
-formatter = logging.Formatter('%(asctime)s - [%(levelname)s]: %(message)s')
+formatter = logging.Formatter("%(asctime)s - [%(levelname)s]: %(message)s")
 
 
 logger.info("-----------------------------------")
-logger.info("Logger initialized with level {}".format(os.getenv("LOGGING_LEVEL", "INFO")))
+logger.info(
+    "Logger initialized with level {}".format(os.getenv("LOGGING_LEVEL", "INFO"))
+)
